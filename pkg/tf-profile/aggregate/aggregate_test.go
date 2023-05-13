58f284b973880711dfc2cdde864b7d02a91ceaad
Quinten Bruynseraede <quinten@dataroots.io>
2023-05-13 19:09:30 +0200

    Fix go vet issues

--- pkg/tf-profile/aggregate/aggregate_test.go
+++ pkg/tf-profile/aggregate/aggregate_test.go
@@ -8,13 +8,17 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// Helper to prevent "struct literal uses unkeyed fields"
+func MkMetric(a int, b float64, c int, d int, e int, f int, g Status, h Status, i Status, j Operation) ResourceMetric {
+	return ResourceMetric{NumCalls: a, TotalTime: b, ModificationStartedIndex: c, ModificationCompletedIndex: d, ModificationStartedEvent: e, ModificationCompletedEvent: f, BeforeStatus: g, AfterStatus: h, DesiredStatus: i, Operation: j}
+}
 func TestAggregateResourceMetricBasic(t *testing.T) {
-	M1 := ResourceMetric{1, 2000, 0, 0, 0, 3, NotCreated, Created, Created, Create}
-	M2 := ResourceMetric{1, 5000, 1, 1, 1, 4, NotCreated, Created, Created, Create}
-	M3 := ResourceMetric{1, 1000, 2, 2, 2, 5, NotCreated, Created, Created, Create}
+	M1 := MkMetric(1, 2000, 0, 0, 0, 3, NotCreated, Created, Created, Create)
+	M2 := MkMetric(1, 5000, 1, 1, 1, 4, NotCreated, Created, Created, Create)
+	M3 := MkMetric(1, 1000, 2, 2, 2, 5, NotCreated, Created, Created, Create)
 
 	Result := aggregateResourceMetrics(M1, M2, M3)
-	Expected := ResourceMetric{3, 8000, 0, 2, 0, 5, NotCreated, Created, Created, Create}
+	Expected := MkMetric(3, 8000, 0, 2, 0, 5, NotCreated, Created, Created, Create)
 	assert.Equalf(t, Expected, Result, "Expected different result after aggregating.")
 }
 
@@ -64,14 +68,14 @@ func TestNoAgg(t *testing.T) {
 func TestBasicAgg(t *testing.T) {
 	In := ParsedLog{
 		Resources: map[string]ResourceMetric{
-			"r1[1]": ResourceMetric{1, 1, 0, 0, 0, 3, NotCreated, Created, Created, Create},
-			"r1[2]": ResourceMetric{1, 1, 1, 1, 1, 4, NotCreated, Created, Created, Create},
-			"r1[3]": ResourceMetric{1, 1, 2, 2, 2, 5, NotCreated, Created, Created, Create},
+			"r1[1]": MkMetric(1, 1, 0, 0, 0, 3, NotCreated, Created, Created, Create),
+			"r1[2]": MkMetric(1, 1, 1, 1, 1, 4, NotCreated, Created, Created, Create),
+			"r1[3]": MkMetric(1, 1, 2, 2, 2, 5, NotCreated, Created, Created, Create),
 		},
 	}
 	Out := ParsedLog{
 		Resources: map[string]ResourceMetric{
-			"r1[*]": ResourceMetric{3, 3, 0, 2, 0, 5, NotCreated, Created, Created, Create},
+			"r1[*]": MkMetric(3, 3, 0, 2, 0, 5, NotCreated, Created, Created, Create),
 		},
 	}
 	Result, err := Aggregate(In)
@@ -82,21 +86,21 @@ func TestBasicAgg(t *testing.T) {
 func TestMixedAgg(t *testing.T) {
 	In := ParsedLog{
 		Resources: map[string]ResourceMetric{
-			"r1[1]":     ResourceMetric{1, 1, 0, 0, 0, 7, NotCreated, Created, Created, Create},
-			"r1[2]":     ResourceMetric{1, 1, 1, 1, 1, 8, NotCreated, Created, Created, Create},
-			"r1[3]":     ResourceMetric{1, 1, 2, 2, 2, 9, NotCreated, Created, Created, Create},
-			"r2[\"a\"]": ResourceMetric{1, 1, 3, 3, 3, 10, NotCreated, Created, Created, Create},
-			"r2[\"b\"]": ResourceMetric{1, 1, 4, 4, 4, 11, NotCreated, Created, Created, Create},
-			"r3":        ResourceMetric{1, 1, 5, 5, 5, 12, NotCreated, Created, Created, Create},
-			"r4":        ResourceMetric{1, 1, 6, 6, 6, 13, NotCreated, Created, Created, Create},
+			"r1[1]":     MkMetric(1, 1, 0, 0, 0, 7, NotCreated, Created, Created, Create),
+			"r1[2]":     MkMetric(1, 1, 1, 1, 1, 8, NotCreated, Created, Created, Create),
+			"r1[3]":     MkMetric(1, 1, 2, 2, 2, 9, NotCreated, Created, Created, Create),
+			"r2[\"a\"]": MkMetric(1, 1, 3, 3, 3, 10, NotCreated, Created, Created, Create),
+			"r2[\"b\"]": MkMetric(1, 1, 4, 4, 4, 11, NotCreated, Created, Created, Create),
+			"r3":        MkMetric(1, 1, 5, 5, 5, 12, NotCreated, Created, Created, Create),
+			"r4":        MkMetric(1, 1, 6, 6, 6, 13, NotCreated, Created, Created, Create),
 		},
 	}
 	Out := ParsedLog{
 		Resources: map[string]ResourceMetric{
-			"r1[*]": ResourceMetric{3, 3, 0, 2, 0, 9, NotCreated, Created, Created, Create},
-			"r2[*]": ResourceMetric{2, 2, 3, 4, 3, 11, NotCreated, Created, Created, Create},
-			"r3":    ResourceMetric{1, 1, 5, 5, 5, 12, NotCreated, Created, Created, Create},
-			"r4":    ResourceMetric{1, 1, 6, 6, 6, 13, NotCreated, Created, Created, Create},
+			"r1[*]": MkMetric(3, 3, 0, 2, 0, 9, NotCreated, Created, Created, Create),
+			"r2[*]": MkMetric(2, 2, 3, 4, 3, 11, NotCreated, Created, Created, Create),
+			"r3":    MkMetric(1, 1, 5, 5, 5, 12, NotCreated, Created, Created, Create),
+			"r4":    MkMetric(1, 1, 6, 6, 6, 13, NotCreated, Created, Created, Create),
 		},
 	}
 	Result, err := Aggregate(In)
@@ -108,37 +112,37 @@ func TestFullAgg(t *testing.T) {
 	In := ParsedLog{
 		Resources: map[string]ResourceMetric{
 			// Can be aggregated on name
-			"module.x.r[1]": ResourceMetric{1, 1, 0, 0, 0, 0, NotCreated, Created, Created, Create},
-			"module.x.r[2]": ResourceMetric{1, 2, 0, 0, 0, 0, NotCreated, Created, Created, Create},
-			"module.x.r[3]": ResourceMetric{1, 3, 0, 0, 0, 0, NotCreated, Created, Created, Create},
+			"module.x.r[1]": MkMetric(1, 1, 0, 0, 0, 0, NotCreated, Created, Created, Create),
+			"module.x.r[2]": MkMetric(1, 2, 0, 0, 0, 0, NotCreated, Created, Created, Create),
+			"module.x.r[3]": MkMetric(1, 3, 0, 0, 0, 0, NotCreated, Created, Created, Create),
 			// With nested modules
-			"module.y[1].module.y[1].r[1]": ResourceMetric{1, 1, 0, 0, 0, 0, NotCreated, Created, Created, Create},
-			"module.y[1].module.y[1].r[2]": ResourceMetric{1, 2, 0, 0, 0, 0, NotCreated, Created, Created, Create},
-			"module.y[1].module.y[1].r[3]": ResourceMetric{1, 3, 0, 0, 0, 0, NotCreated, Created, Created, Create},
-			// With ModificationStartedIndex and ModificationCompletedIndex
-			"module.z[1].module.z[1].r[1]": ResourceMetric{1, 3, 2, 1, 0, 0, NotCreated, Created, Created, Create},
-			"module.z[1].module.z[1].r[2]": ResourceMetric{1, 2, 3, 5, 0, 0, NotCreated, Created, Created, Create},
-			"module.z[1].module.z[1].r[3]": ResourceMetric{1, 1, 5, 9, 0, 0, NotCreated, Created, Created, Create},
+			"module.y[1].module.y[1].r[1]": MkMetric(1, 1, 0, 0, 0, 0, NotCreated, Created, Created, Create),
+			"module.y[1].module.y[1].r[2]": MkMetric(1, 2, 0, 0, 0, 0, NotCreated, Created, Created, Create),
+			"module.y[1].module.y[1].r[3]": MkMetric(1, 3, 0, 0, 0, 0, NotCreated, Created, Created, Create),
+			// With ModificationStartedIndexMkMetric(onCompletedIndex
+			"module.z[1].module.z[1].r[1]": MkMetric(1, 3, 2, 1, 0, 0, NotCreated, Created, Created, Create),
+			"module.z[1].module.z[1].r[2]": MkMetric(1, 2, 3, 5, 0, 0, NotCreated, Created, Created, Create),
+			"module.z[1].module.z[1].r[3]": MkMetric(1, 1, 5, 9, 0, 0, NotCreated, Created, Created, Create),
 			// Mixed states
-			"module.a[1].module.b[1].r[1]": ResourceMetric{1, 3, 2, 1, 0, 0, NotCreated, Created, Created, Create},
-			"module.a[1].module.b[1].r[2]": ResourceMetric{1, 2, 3, 5, 0, 0, NotCreated, Failed, Created, Create},
-			"module.a[1].module.b[1].r[3]": ResourceMetric{1, 1, 5, 9, 0, 0, NotCreated, Created, Created, Create},
-			"module.a[1].module.b[1].r[4]": ResourceMetric{1, 1, 5, 9, 0, 0, NotCreated, Created, Created, Create},
+			"module.a[1].module.b[1].r[1]": MkMetric(1, 3, 2, 1, 0, 0, NotCreated, Created, Created, Create),
+			"module.a[1].module.b[1].r[2]": MkMetric(1, 2, 3, 5, 0, 0, NotCreated, Failed, Created, Create),
+			"module.a[1].module.b[1].r[3]": MkMetric(1, 1, 5, 9, 0, 0, NotCreated, Created, Created, Create),
+			"module.a[1].module.b[1].r[4]": MkMetric(1, 1, 5, 9, 0, 0, NotCreated, Created, Created, Create),
 			// Not agg'able
-			"random_resource":  ResourceMetric{1, 1, 5, 9, 1, 1, NotCreated, Created, Created, Create},
-			"random_resource2": ResourceMetric{1, 1, 5, 9, 2, 2, NotCreated, Failed, Created, Create},
-			"random_resource3": ResourceMetric{1, 1, 5, 9, 3, 3, NotCreated, Failed, Created, Create},
+			"random_resource":  MkMetric(1, 1, 5, 9, 1, 1, NotCreated, Created, Created, Create),
+			"random_resource2": MkMetric(1, 1, 5, 9, 2, 2, NotCreated, Failed, Created, Create),
+			"random_resource3": MkMetric(1, 1, 5, 9, 3, 3, NotCreated, Failed, Created, Create),
 		},
 	}
 	Out := ParsedLog{
 		Resources: map[string]ResourceMetric{
-			"module.x.r[*]":                ResourceMetric{3, 6, 0, 0, 0, 0, NotCreated, Created, Created, Create},
-			"module.y[1].module.y[1].r[*]": ResourceMetric{3, 6, 0, 0, 0, 0, NotCreated, Created, Created, Create},
-			"module.z[1].module.z[1].r[*]": ResourceMetric{3, 6, 2, 9, 0, 0, NotCreated, Created, Created, Create},
-			"module.a[1].module.b[1].r[*]": ResourceMetric{4, 7, 2, 9, 0, 0, NotCreated, Multiple, Created, Create},
-			"random_resource":              ResourceMetric{1, 1, 5, 9, 1, 1, NotCreated, Created, Created, Create},
-			"random_resource2":             ResourceMetric{1, 1, 5, 9, 2, 2, NotCreated, Failed, Created, Create},
-			"random_resource3":             ResourceMetric{1, 1, 5, 9, 3, 3, NotCreated, Failed, Created, Create},
+			"module.x.r[*]":                MkMetric(3, 6, 0, 0, 0, 0, NotCreated, Created, Created, Create),
+			"module.y[1].module.y[1].r[*]": MkMetric(3, 6, 0, 0, 0, 0, NotCreated, Created, Created, Create),
+			"module.z[1].module.z[1].r[*]": MkMetric(3, 6, 2, 9, 0, 0, NotCreated, Created, Created, Create),
+			"module.a[1].module.b[1].r[*]": MkMetric(4, 7, 2, 9, 0, 0, NotCreated, Multiple, Created, Create),
+			"random_resource":              MkMetric(1, 1, 5, 9, 1, 1, NotCreated, Created, Created, Create),
+			"random_resource2":             MkMetric(1, 1, 5, 9, 2, 2, NotCreated, Failed, Created, Create),
+			"random_resource3":             MkMetric(1, 1, 5, 9, 3, 3, NotCreated, Failed, Created, Create),
 		},
 	}
 	Result, err := Aggregate(In)
