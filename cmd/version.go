3ebd31e742a59f061034719002e29ce191a6e987
Quinten Bruynseraede <quinten@dataroots.io>
2023-05-26 23:04:39 +0200

    Implement 'version' command

---
+++ cmd/version.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version and exit.",
+	Long:  `Print the version and exit.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("tf-profile v0.2.1")
+	},
+}
